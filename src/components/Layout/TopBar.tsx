90b6f2986397aba39d6f1c0183806938afbcf59b
Sérgio Danilo <sergio.danilo@gmail.com>
2021-02-14 23:30:50 +0000

    feat: reafactoring my name and photo

--- src/components/Layout/TopBar.tsx
+++ src/components/Layout/TopBar.tsx
@@ -26,11 +26,13 @@ export default function TopBar() {
     <div className="header-intro theme-bg-primary text-white py-5">
       <div className="container">
         <div className="profile-teaser media flex-column flex-md-row">
-          <Img fixed={data.image.childImageSharp.fixed}
-            className="profile-image mb-3 mb-md-0 mr-md-5 ml-md-0 rounded mx-auto"/>
+          <Img
+            fixed={data.image.childImageSharp.fixed}
+            className="profile-image mb-3 mb-md-0 mr-md-5 ml-md-0 rounded mx-auto"
+          />
           <div className="media-body text-center text-md-left">
             <div className="lead">Hello, my name is</div>
-            <h2 className="mt-0 display-4 font-weight-bold">Steve Doe</h2>
+            <h2 className="mt-0 display-4 font-weight-bold">Sérgio Holanda</h2>
             <div className="bio mb-3">I'm a full-stack developer specialised in frontend and backend development for
               complex scalable web apps. I write about web development on <a className="link-on-bg" href="blog.html">my
                 blog</a> and regularly <a className="link-on-bg" href="talks.html">speak</a> at various web
